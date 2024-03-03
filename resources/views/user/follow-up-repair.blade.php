3007ccee387d8304b6077947ee36c288fc8c0e75
art <thanakorn.on@rmuti.ac.th>
2024-03-03 22:06:27 +0700

    new follow up repir

--- resources/views/user/follow-up-repair.blade.php
+++ resources/views/user/follow-up-repair.blade.php
@@ -3,50 +3,6 @@
 @vite('resources\css\followUprepir.css')
 
 @section('content')
-
-    {{--  <div class="card mb-3" style="max-width: 800px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScJ7Enh0Zg8jVlRleiXNeettNBiVhGDGe5BA&usqp=CAU"
-             class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">ติดตามสถานะการแจ้งซ่อม</h5>
-              <p class="card-text">กรอกหมายเลขเเท็ก 13 หลัก [ตัวอย่าง : EF582568151TH]</p>
-              <div class="row">
-                <div class="search">
-                    <input type="text" class="form-control" placeholder="*กรุณาระบบหมายเลขเเท็กเพื่อค้นหา">
-                    <button class="btn btn-primary">ค้นหา</button>
-                </div>
-              </div>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-    </div>  --}}
-
-    {{--  <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-sm-8">
-                <div class="card">
-                    <h5 class="card-title">ติดตามสถานะการแจ้งซ่อม</h5>
-                        <span class="text text-primary">กรอกหมายเลขเเท็ก 13 หลัก [ตัวอย่าง : EF582568151TH]</span>
-
-                            <div class="input-group mb-6">
-                                <input type="text" class="form-control me-2" id="search" placeholder="กรุณากรองหมายเลขเเท็กเเจ้งซ่อมเพื่อค้นหา">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" onclick="filterRepairs()">ค้นหา</button>
-                                    </div>
-                                </input>
-                            </div>
-
-                    <p id="demo"></p>
-                </div>
-            </div>
-        </div>
-    </div>  --}}
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-8">
@@ -54,28 +10,20 @@
                     <div class="card-body">
                         <h5 class="card-title">ติดตามสถานะการแจ้งซ่อม</h5>
                         <p class="card-text text-primary">กรอกหมายเลขเเท็ก 13 หลัก [ตัวอย่าง : EF582568151TH]</p>
-
-
                             <div class="row justify-content-center align-items-center">
                                  <input type="text" class="form-control w-75" id="search" placeholder="กรองหมายเลขเเท็กเพื่อค้นหา">
                                  <div class="col-auto">
                                         <button class="btn btn-primary" onclick="filterRepairs()">ค้นหา</button>
                                     </div>
                             </div>
-
-
-
-
                     </div>
-
                 </div>
-
                 {{--  <p id="demo"></p>  --}}
             </div>
         </div>
         <br>
 
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" style="display: none" id="list">
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-body">
@@ -93,18 +41,44 @@
                                     <h6 class="text-danger"><p id="timeCreatedAt"></p></h6>
                                 </div>
                             </div>
-                            <div class="row text-center">
-                                <div class="col-sm-12">
-                                    <h6>351348646435453</h6>
-                                </div>
-                            </div>
 
-                            </div>
+                                <div class="col-sm-12">
+                                    <div class="container">
+                                        <input type="radio" class="profile" id="radio1">
+                                        {{--  <input type="radio" class="settings" id="radio2">  --}}
+                                        <input type="radio" class="posts" id="radio2">
+                                        <input type="radio" class="books"  id="radio3">
+                                    <div class="head">
+                                        <ul class="nav">
+                                            <li class="st st1 active">
+                                                <h2 class="inner">เเจ้งซ่อม</h2>
+                                            </li>
+                                            {{--  <li class="st st2">
+                                                <h2 class="inner">รับเเเจ้ง</h2>
+                                            </li>  --}}
+                                            <li class="st st2">
+                                                <h2 class="inner">รอดำเนินการ</h2>
+                                            </li>
+                                            <li class="st st3">
+                                                <h2 class="inner">เสร็จสิ้น</h2>
+                                            </li>
+                                        </ul>
+                                        <div class="line">
+                                            <span></span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                </div>
+
+                    </div>
                 </div>
             </div>
 
         </div>
     </div>
+
+
 @endsection
 
 @section('script')
@@ -115,6 +89,10 @@ cont
             const resultRepairs = document.getElementById("search");
 
             const filterData = repairData.find((word) => word.tag_repair === resultRepairs.value);
+            document.getElementById("list").style.display='block';
+            if(filterData.status_repair == "รอดำเนินการ"){
+                document.getElementById("radio2").checked = true;
+            }
 
             const dateObject = new Date(filterData.created_at);
 
